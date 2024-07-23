708859053f9a9235c58a191bd69feb60bf3378d6
Marlon <marlon_menezes@outlook.com>
2024-07-22 21:41:40 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import GameOfThrones from './components/GameOfThronesCards.vue';
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <Suspense>
+      <template #default>
+        <GameOfThrones />
+      </template>
+      <template #fallback>
+        <div class="loading_wrapper">
+          <p class="loading_text">Loading <span class="loading_dots">...</span></p>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
+
+<style scoped>
+  .loading_text {
+    font-size: 1.2rem;
+  }
+
+  .loading_dots {
+    animation: loading 1s infinite;
+  }
+
+  @keyframes loading {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+</style>
---
+++ src/components/GameOfThronesCards.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import axios from 'axios';
+
+  const response = await axios.get('https://thronesapi.com/api/v2/Characters')
+  console.log(response.data)
+</script>
+
+<template>
+  <div>
+    <h1>Game of Thrones Cards</h1>
+    <h2>{{response.length}}</h2>
+  </div>
+</template>
